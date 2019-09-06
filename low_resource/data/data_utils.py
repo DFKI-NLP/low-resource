e878a486499b17bb3032f14ccba2326725274011
Arne Binder <arne.b.binder@gmail.com>
2019-09-06 15:00:04 +0200

    data preparation: do not convert pos tags; separate json to jsonl conversion from splitting

--- low_resource/data/data_utils.py
+++ low_resource/data/data_utils.py
@@ -6,35 +6,62 @@
 @author: leonhard.hennig@dfki.de
 """
 import json
-from random import shuffle
+import os
+
 import numpy as np
 
 
 def to_iob1(instance):
+    # encode all tags as "inside" (I-) tags
     for t_idx in range(len(instance['ner'])):
         if instance['ner'][t_idx] != 'O':
             instance['ner'][t_idx] = 'I-' + instance['ner'][t_idx]
-    for t_idx in range(len(instance['pos'])):
-        if instance['pos'][t_idx] != 'O':
-            instance['pos'][t_idx] = 'I-' + instance['pos'][t_idx]
+    #for t_idx in range(len(instance['pos'])):
+    #    if instance['pos'][t_idx] != 'O':
+    #        instance['pos'][t_idx] = 'I-' + instance['pos'][t_idx]
     return instance
 
 
-def convert_json_to_jsonl(path, train, dev):
-    '''
-    hacky one-off conversion script to create two jsonl files from train_distant.json
-    '''
-    with open(path) as f:
+def convert_json_to_jsonl(path_in, path_out=None, min_tokens=6, force_recreate=False):
+    """
+    create json line file from json file (has to contain an array of instances)
+    """
+    print(f'convert to jsonl {path_in} ...')
+    if path_out is None:
+        path_out = (path_in + 'l') if path_in.endswith('.json') else path_in + '.jsonl'
+    if os.path.exists(path_out):
+        if force_recreate:
+            print(f'WARNING: {path_out} already exists. File will be overwritten!')
+        else:
+            print(f'WARNING: {path_out} already exists. File will NOT be recreated!')
+            return path_out
+    n = 0
+    n_discarded = 0
+    with open(path_in) as f:
         data = json.load(f)
-        indices = np.random.permutation(len(data))
-        with open(train, 'w') as out_train:
-            for idx in indices[:int(np.round(len(data) * 0.8))]:
-                if len(data[idx]['token']) > 5:
-                    out_train.write(json.dumps(to_iob1(data[idx])) + '\n')
-        with open(dev, 'w') as out_dev:
-            for idx in indices[int(np.round(len(data) * 0.8)):]:
-                if len(data[idx]['token']) > 5:
-                    out_dev.write(json.dumps(to_iob1(data[idx])) + '\n')
+        with open(path_out, 'w') as out_f:
+            for inst in data:
+                if len(inst['token']) >= min_tokens:
+                    out_f.write(json.dumps(to_iob1(inst)) + '\n')
+                    n += 1
+                else:
+                    n_discarded += 1
+    print(f'wrote {n} instances to {path_out} (discard {n_discarded} instances because len(tokens) < {min_tokens})')
+    return path_out
+
+
+def create_train_dev_split(path, train, dev, proportion_train=0.8):
+    with open(path) as f:
+        lines = f.readlines()
+    indices = np.random.permutation(len(lines))
+    k = int(np.round(len(lines) * proportion_train))
+    open(train, 'w').writelines((lines[idx] for idx in indices[:k]))
+    print(f'created {train} ({k} instances)')
+    open(dev, 'w').writelines((lines[idx] for idx in indices[k:]))
+    print(f'created {dev} ({len(indices) - k} instances)')
+
 
 if __name__ == '__main__':
-    convert_json_to_jsonl('../../data/train_distant.json', '../../data/train_distant.jsonl', '../../data/dev_distant.jsonl')
+    #path_out = convert_json_to_jsonl('../../data/distantly_labeled/amazon_wdc_washer_distant.json')
+    path_out = convert_json_to_jsonl('../../data/distantly_labeled/lenovo_distant.json')
+    create_train_dev_split(path_out, '../../data/train_distant.jsonl', '../../data/dev_distant.jsonl')
