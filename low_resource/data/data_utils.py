0c856b8197d3796353e07c8489765da44ba795f5
Arne Binder <arne.b.binder@gmail.com>
2019-09-19 15:56:23 +0200

    use BIO encoding

--- low_resource/data/data_utils.py
+++ low_resource/data/data_utils.py
@@ -11,24 +11,29 @@ import os
 import numpy as np
 
 
-def to_iob1(instance):
+def to_iob1(instance, tag_type):
     # encode all tags as "inside" (I-) tags
-    for t_idx in range(len(instance['ner'])):
-        if instance['ner'][t_idx] != 'O':
-            instance['ner'][t_idx] = 'I-' + instance['ner'][t_idx]
+    last_t = None
+    for t_idx, t in enumerate(instance[tag_type]):
+        if instance[tag_type][t_idx] != 'O':
+            if t != last_t:
+                instance[tag_type][t_idx] = 'B-' + t
+            else:
+                instance[tag_type][t_idx] = 'I-' + t
+        last_t = t
     #for t_idx in range(len(instance['pos'])):
     #    if instance['pos'][t_idx] != 'O':
     #        instance['pos'][t_idx] = 'I-' + instance['pos'][t_idx]
     return instance
 
 
-def convert_json_to_jsonl(path_in, path_out=None, min_tokens=6, force_recreate=False):
+def convert_json_to_jsonl_bio(path_in, path_out=None, min_tokens=6, force_recreate=False):
     """
     create json line file from json file (has to contain an array of instances)
     """
     print(f'convert to jsonl {path_in} ...')
     if path_out is None:
-        path_out = (path_in + 'l') if path_in.endswith('.json') else path_in + '.jsonl'
+        path_out = (path_in[:len(path_in)-5] + '_bio.jsonl') if path_in.endswith('.json') else path_in + '_bio.jsonl'
     if os.path.exists(path_out):
         if force_recreate:
             print(f'WARNING: {path_out} already exists. File will be overwritten!')
@@ -42,7 +47,7 @@ def convert_json_to_jsonl(path_in, path_out=None, min_tokens=6, force_recreate=F
         with open(path_out, 'w') as out_f:
             for inst in data:
                 if len(inst['token']) >= min_tokens:
-                    out_f.write(json.dumps(to_iob1(inst)) + '\n')
+                    out_f.write(json.dumps(to_iob1(inst, tag_type='ner')) + '\n')
                     n += 1
                 else:
                     n_discarded += 1
@@ -62,6 +67,6 @@ def create_train_dev_split(path, train, dev, proportion_train=0.8):
 
 
 if __name__ == '__main__':
-    path_out = convert_json_to_jsonl('../../data/distantly_labeled/amazon_wdc_washer_distant.json')
+    path_out = convert_json_to_jsonl_bio('../../data/distantly_labeled/amazon_wdc_washer_distant.json')
     #path_out = convert_json_to_jsonl('../../data/distantly_labeled/lenovo_distant.json')
     create_train_dev_split(path_out, '../../data/train_distant.jsonl', '../../data/dev_distant.jsonl')
